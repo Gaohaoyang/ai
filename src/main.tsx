2c67eabb8c86b84f602991eb6077657b82123148
Haoyang Gao <gaohaoyang126@126.com>
2024-05-14 17:54:41 -0400

    feat: 🎸 update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import 'normalize.css'
 import './index.css'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const lazyWrap = (factory: () => Promise<any>) => {
+  return async () => {
+    const page = await factory()
+    // https://reactrouter.com/en/main/route/lazy
+    return {
+      Component: page.default || page.Component,
+      ErrorBoundary: page.ErrorBoundary,
+      loader: page.loader,
+    }
+  }
+}
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: '/demo',
+    lazy: lazyWrap(() => import('@/pages/Demo')),
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />)
