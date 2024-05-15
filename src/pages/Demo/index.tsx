07f34b8e3b3d78a43e63794b6b87989eeac0d3d1
Haoyang Gao <gaohaoyang126@126.com>
2024-05-15 11:21:55 -0400

    feat: 🎸 demo

--- src/pages/Demo/index.tsx
+++ src/pages/Demo/index.tsx
@@ -1,4 +1,19 @@
+import { pipeline } from '@xenova/transformers'
+import { useEffect } from 'react'
+
 const Demo = () => {
+  const test = async () => {
+    // Allocate a pipeline for sentiment-analysis
+    const pipe = await pipeline('sentiment-analysis')
+    const out = await pipe('我喜欢你')
+    console.log(out)
+    // [{'label': 'POSITIVE', 'score': 0.999817686}]
+  }
+
+  useEffect(() => {
+    test()
+  }, [])
+
   return <div>Demo</div>
 }
 
